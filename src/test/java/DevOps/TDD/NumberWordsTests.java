f29e5dc9189bcea48d53c964b53182b77d8dcf1f
UntamedArian <37007773+UntamedArian@users.noreply.github.com>
2019-01-06 16:38:04 +0100

    added files to the TDDN

---
+++ src/test/java/DevOps/TDD/NumberWordsTests.java
@@ -0,0 +1,19 @@
+package DevOps.TDD;
+
+import org.junit.*;
+
+import DevOps.TDD.NumberWords;
+
+public class NumberWordsTests {
+	private NumberWords numberWords ;
+	
+	@Before
+	public void setup() {
+		numberWords = new NumberWords() ;
+	}
+
+	@Test
+	public void numberOutOfRangeReturnsError() {
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
+	}
+}
